ef13907dbcf6176983e7c411b67b51a5372bc485
Ryan Groff <rgroff200@cable.comcast.com>
2025-03-13 14:31:59 -0400

    keep changing setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name='puppetfile-dependency-check',
     version='1.0.0',
     description='Checks Puppetfile dependencies against Puppet Forge.',
-    packages=find_packages(), # will find all packages inside of the root.
+    packages=find_packages(),
     install_requires=[
         'semver',
         'requests',
     ],
     entry_points={
         'console_scripts': [
-            'check-puppetfile-dependencies=pre_commit_hooks.check-puppetfile-dependencies:main', #Optional, if you want to run from the command line
+            'check-puppetfile-dependencies = pre_commit_hooks.check_puppetfile_dependencies:main',  # Corrected entry point
         ],
     },
 )
